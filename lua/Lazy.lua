09f967e5c876ee4a8e6ea4bb9cec0b8bb32615e4
Niamh <Niamh.Ferns@proton.me>
2023-08-25 11:02:41 +1200

    Better theme management.
    See nekovim-user-config for where to set theme. CURRENTLY NOT PROTECTED...

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -1,13 +1,13 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,40 +17,41 @@ vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappin
 local lazy = {}
 
 lazy.modules = {
-	-- Depends
-	{ import = "modules.depends" },
+  -- Depends
+  { import = "modules.depends" },
 
-	-- Core
-	{ import = "modules.core" },
-	{ import = "modules.core.debug" },
-	{ import = "modules.core.flow" },
-	{ import = "modules.core.motions" },
-	{ import = "modules.core.git" },
-	{ import = "modules.core.languages" },
-	{ import = "modules.core.legible" },
-	{ import = "modules.core.lsp" },
-	{ import = "modules.core.debug" },
+  -- Core
+  { import = "modules.core" },
+  { import = "modules.core.debug" },
+  { import = "modules.core.flow" },
+  { import = "modules.core.motions" },
+  { import = "modules.core.git" },
+  { import = "modules.core.languages" },
+  { import = "modules.core.legible" },
+  { import = "modules.core.lsp" },
+  { import = "modules.core.debug" },
 
-	-- Looks
-	{ import = "modules.looks" },
-	{ import = "modules.looks.themes" },
-	{ import = "modules.looks.ui" },
+  -- Looks
+  { import = "modules.looks" },
+  { import = "modules.looks.look-and-feel" },
+  { import = "modules.looks.themes" },
+  { import = "modules.looks.ui" },
 
-	-- Writing
-	{ import = "modules.writing" },
-	{ import = "modules.writing.neorg" },
+  -- Writing
+  { import = "modules.writing" },
+  { import = "modules.writing.neorg" },
 
-	-- Tools
-	{ import = "modules.tools" },
+  -- Tools
+  { import = "modules.tools" },
 }
 
 lazy.opts = {
-	install = { colorscheme = { require("modules.looks.themes.primary_theme").name } },
-	defaults = { lazy = true, version = "57cce98dfdb2f2dd05a0567d89811e6d0505e13b" },
-	ui = { wrap = "true" },
-	checker = { enabled = true },
-	change_detection = { enabled = true },
-	debug = false,
+  install = { colorscheme = { require("user-options").primary_theme } },
+  defaults = { lazy = true, version = "57cce98dfdb2f2dd05a0567d89811e6d0505e13b" },
+  ui = { wrap = "true" },
+  checker = { enabled = true },
+  change_detection = { enabled = true },
+  debug = false,
 }
 
 -- load lazy
--- lua/modules/looks/themes/tokyonight.lua
+++ lua/modules/looks/themes/tokyonight.lua
@@ -1,6 +1,22 @@
 local M = {
-	"folke/tokyonight.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  "folke/tokyonight.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000,
 }
+
+local valid_names = {
+  ["tokyonight"] = true,
+  ["tokyonight-day"] = true,
+  ["tokyonight-moon"] = true,
+  ["tokyonight-night"] = true,
+  ["tokyonight-storm"] = true,
+}
+
+M.config = function(_, opts)
+  require("tokyonight").setup(opts)
+  if valid_names[require("user-options").primary_theme] then
+    pcall(vim.cmd.colorscheme, require("user-options").primary_theme)
+  end
+end
 
 return M
