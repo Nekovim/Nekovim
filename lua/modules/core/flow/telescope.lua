eac4fbd42f9f574abcc876cf4e60045c48abf80c
Niamh <Niamh.Ferns@proton.me>
2023-08-25 11:01:46 +1200

    Cleaner notifications for noice and better vim.ui.select
    Mini now displays for a decent period and uses telescope for vim.ui.select calls. Don't display lsp spam for time being.

--- lua/modules/core/flow/telescope.lua
+++ lua/modules/core/flow/telescope.lua
@@ -1,112 +1,119 @@
 local M = {
-	"nvim-telescope/telescope.nvim",
-	event = "Bufenter",
-	cmd = { "Telescope" },
-	dependencies = {
-		{
-			"ahmedkhalf/project.nvim",
-		},
-		{
-			"nvim-telescope/telescope-file-browser.nvim",
-			dependencies = { "nvim-lua/plenary.nvim" },
-		},
-	},
+  "nvim-telescope/telescope.nvim",
+  event = "Bufenter",
+  cmd = { "Telescope" },
+  dependencies = {
+    {
+      "ahmedkhalf/project.nvim",
+    },
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+  },
 }
 
-local actions = require("telescope.actions")
+local actions = require "telescope.actions"
 
 M.opts = {
-	defaults = {
-		prompt_prefix = " ",
-		selection_caret = " ",
-		path_display = { "smart" },
-		file_ignore_patterns = { ".git/", "node_modules" },
-		mappings = {
-			i = {
-				["<C-x>"] = actions.select_horizontal,
-				["<C-v>"] = actions.select_vertical,
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+    mappings = {
+      i = {
+        ["<C-x>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
 
-				["<C-n>"] = actions.move_selection_next,
-				["<C-e>"] = actions.move_selection_previous,
+        ["<C-n>"] = actions.move_selection_next,
+        ["<C-e>"] = actions.move_selection_previous,
 
-				["<PageDown>"] = actions.preview_scrolling_up,
-				["<PageUp>"] = actions.preview_scrolling_down,
-				["<C-PageDown>"] = actions.cycle_history_next,
-				["<C-PageUp>"] = actions.cycle_history_prev,
+        ["<PageDown>"] = actions.preview_scrolling_up,
+        ["<PageUp>"] = actions.preview_scrolling_down,
+        ["<C-PageDown>"] = actions.cycle_history_next,
+        ["<C-PageUp>"] = actions.cycle_history_prev,
 
-				["<C-u>"] = actions.close,
-				["<Esc>"] = actions.close,
-			},
-			n = {
-				["<C-x>"] = actions.select_horizontal,
-				["<C-v>"] = actions.select_vertical,
+        ["<C-u>"] = actions.close,
+        ["<Esc>"] = actions.close,
+      },
+      n = {
+        ["<C-x>"] = actions.select_horizontal,
+        ["<C-v>"] = actions.select_vertical,
 
-				["n"] = actions.move_selection_next,
-				["e"] = actions.move_selection_previous,
+        ["n"] = actions.move_selection_next,
+        ["e"] = actions.move_selection_previous,
 
-				["<PageDown>"] = actions.preview_scrolling_up,
-				["<PageUp>"] = actions.preview_scrolling_down,
-				["<C-PageDown>"] = actions.cycle_history_next,
-				["<C-PageUp>"] = actions.cycle_history_prev,
+        ["<PageDown>"] = actions.preview_scrolling_up,
+        ["<PageUp>"] = actions.preview_scrolling_down,
+        ["<C-PageDown>"] = actions.cycle_history_next,
+        ["<C-PageUp>"] = actions.cycle_history_prev,
 
-				["gg"] = actions.move_to_top,
-				["M"] = actions.move_to_middle,
-				["G"] = actions.move_to_bottom,
+        ["gg"] = actions.move_to_top,
+        ["M"] = actions.move_to_middle,
+        ["G"] = actions.move_to_bottom,
 
-				["<C-u>"] = actions.close,
-				["<Esc>"] = actions.close,
-			},
-		},
-	},
+        ["<C-u>"] = actions.close,
+        ["<Esc>"] = actions.close,
+      },
+    },
+  },
 
-	extensions = {
-		file_browser = {
-			mappings = {
-				i = {
-					["<C-x>"] = actions.select_horizontal,
-					["<C-v>"] = actions.select_vertical,
+  extensions = {
+    file_browser = {
+      mappings = {
+        i = {
+          ["<C-x>"] = actions.select_horizontal,
+          ["<C-v>"] = actions.select_vertical,
 
-					["<C-n>"] = actions.move_selection_next,
-					["<C-e>"] = actions.move_selection_previous,
+          ["<C-n>"] = actions.move_selection_next,
+          ["<C-e>"] = actions.move_selection_previous,
 
-					["<PageDown>"] = actions.preview_scrolling_up,
-					["<PageUp>"] = actions.preview_scrolling_down,
-					["<C-PageDown>"] = actions.cycle_history_next,
-					["<C-PageUp>"] = actions.cycle_history_prev,
+          ["<PageDown>"] = actions.preview_scrolling_up,
+          ["<PageUp>"] = actions.preview_scrolling_down,
+          ["<C-PageDown>"] = actions.cycle_history_next,
+          ["<C-PageUp>"] = actions.cycle_history_prev,
 
-					["<C-u>"] = actions.close,
-					["<Esc>"] = actions.close,
-				},
-				n = {
-					["<C-x>"] = actions.select_horizontal,
-					["<C-v>"] = actions.select_vertical,
+          ["<C-u>"] = actions.close,
+          ["<Esc>"] = actions.close,
+        },
+        n = {
+          ["<C-x>"] = actions.select_horizontal,
+          ["<C-v>"] = actions.select_vertical,
 
-					["n"] = actions.move_selection_next,
-					["e"] = actions.move_selection_previous,
+          ["n"] = actions.move_selection_next,
+          ["e"] = actions.move_selection_previous,
 
-					["<PageDown>"] = actions.preview_scrolling_up,
-					["<PageUp>"] = actions.preview_scrolling_down,
-					["<C-PageDown>"] = actions.cycle_history_next,
-					["<C-PageUp>"] = actions.cycle_history_prev,
+          ["<PageDown>"] = actions.preview_scrolling_up,
+          ["<PageUp>"] = actions.preview_scrolling_down,
+          ["<C-PageDown>"] = actions.cycle_history_next,
+          ["<C-PageUp>"] = actions.cycle_history_prev,
 
-					["gg"] = actions.move_to_top,
-					["M"] = actions.move_to_middle,
-					["G"] = actions.move_to_bottom,
+          ["gg"] = actions.move_to_top,
+          ["M"] = actions.move_to_middle,
+          ["G"] = actions.move_to_bottom,
 
-					["<C-u>"] = actions.close,
-					["<Esc>"] = actions.close,
-				},
-			},
+          ["<C-u>"] = actions.close,
+          ["<Esc>"] = actions.close,
+        },
+      },
 
-			hijack_netrw = true,
-		},
-	},
+      hijack_netrw = true,
+    },
+    ["ui-select"] = {
+      require("telescope.themes").get_cursor(),
+    },
+  },
 }
 
 function M.config(_, opts)
-	require("telescope").setup(opts)
-	require("telescope").load_extension("file_browser")
-	require("telescope").load_extension("harpoon")
+  require("telescope").setup(opts)
+  require("telescope").load_extension "file_browser"
+  require("telescope").load_extension "ui-select"
+  require("telescope").load_extension "harpoon"
 end
 
 return M
--- lua/modules/looks/ui/noice.lua
+++ lua/modules/looks/ui/noice.lua
@@ -1,65 +1,68 @@
 local M = {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
 }
 
 M.opts = {
-	lsp = {
-		hover = {
-			enabled = false,
-		},
-		signature = {
-			enabled = false,
-		},
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true,
-		},
-	},
-	-- you can enable a preset for easier configuration
-	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
-		command_palette = true, -- position the cmdline and popupmenu together
-		long_message_to_split = true, -- long messages will be sent to a split
-		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
-	},
+  lsp = {
+    progress = {
+      enabled = false,
+    },
+    hover = {
+      enabled = false,
+    },
+    signature = {
+      enabled = false,
+    },
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
 
-	messages = {
-		enabled = true,
-		view = "mini",
-		view_error = "messages",
-		view_warn = "mini",
-		view_history = "messages",
-		view_search = "virtualtext"
-	},
+  messages = {
+    enabled = true,
+    view = "mini",
+    view_error = "messages",
+    view_warn = "mini",
+    view_history = "messages",
+    view_search = "virtualtext",
+  },
 
-	notify = {
-		enabled = true,
-		view = "mini"
-	},
+  notify = {
+    enabled = true,
+    view = "mini",
+  },
 
-	views = {
-		mini = {
-			timeout = 5000,
-		},
-	}
+  views = {
+    mini = {
+      timeout = 5000,
+    },
+  },
 }
 
 M.config = function(_, opts)
-	require("notify").setup({ 
-		render = "compact",
-		stages = "slide",
-		background_colour = "#26233a",
-		timeout = 5000,
-	})
-	require("noice").setup(opts)
+  require("notify").setup {
+    render = "compact",
+    stages = "slide",
+    background_colour = "#26233a",
+    timeout = 5000,
+  }
+  require("noice").setup(opts)
 end
 
 return M
