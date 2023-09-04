098c7958bc2fc5ec2e88ac5f6b528936e42afb9c
Niamh <Niamh.Ferns@proton.me>
2023-09-04 13:03:57 +1200

    added oxocarbon.nvim theme.
    closes #28

---
+++ lua/modules/looks/themes/oxocarbon.lua
@@ -0,0 +1,18 @@
+local M = {
+  "nyoom-engineering/oxocarbon.nvim",
+  priority = 1000,
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+}
+
+local valid_names = {
+  ["oxocarbon"] = true,
+}
+
+M.config = function(_, opts)
+  require("gruvbox").setup(opts)
+  if valid_names[require("user-options").primary_theme] then
+    pcall(vim.cmd.colorscheme, require("user-options").primary_theme)
+  end
+end
+
+return M
