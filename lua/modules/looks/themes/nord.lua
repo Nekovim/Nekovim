4bc8e8c43ce7cd9213d98023b89f68f1e8e270d5
Niamh <Niamh.Ferns@proton.me>
2023-09-04 13:12:45 +1200

    added nord.nvim theme.
    closes #25
    
    Still need to add configuration for nord but works for now.

---
+++ lua/modules/looks/themes/nord.lua
@@ -0,0 +1,17 @@
+local M = {
+  "shaunsingh/nord.nvim",
+  priority = 1000,
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+}
+
+local valid_names = {
+  ["nord"] = true,
+}
+
+M.config = function(_, opts)
+  if valid_names[require("user-options").primary_theme] then
+    pcall(vim.cmd.colorscheme, require("user-options").primary_theme)
+  end
+end
+
+return M
