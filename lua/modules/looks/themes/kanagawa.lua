cc47dad8463054c09c90b05e02a54bead6fdc38e
Niamh <Niamh.Ferns@proton.me>
2023-09-07 14:07:11 +1200

    Updated kanagawa options.
    Wave is now default theme again.

--- lua/modules/looks/themes/kanagawa.lua
+++ lua/modules/looks/themes/kanagawa.lua
@@ -46,7 +46,7 @@ M.opts = {
   end,
   theme = "wave", -- Load "wave" theme when 'background' option is not set
   background = { -- map the value of 'background' option to a theme
-    dark = "dragon", -- try "dragon" !
+    dark = "wave", -- try "dragon" !
     light = "lotus",
   },
 }
