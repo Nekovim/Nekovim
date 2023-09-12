0d841e072010f8141a5eb171dd8658e19ad76d97
Niamh <Niamh.Ferns@proton.me>
2023-09-12 19:22:07 +1200

    Rust tools now uses codelldb provided by mason.
    Previously would point to nothing and user had to manually install.

--- lua/modules/core/languages/rust-tools.lua
+++ lua/modules/core/languages/rust-tools.lua
@@ -1,5 +1,5 @@
 -- Update this path
-local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.9.0/"
+local extension_path = vim.fn.stdpath "data" .. "/home/niamh/.local/share/nvim/mason/packages/codelldb/extension"
 local codelldb_path = extension_path .. "adapter/codelldb"
 local liblldb_path = extension_path .. "lldb/lib/liblldb.so" -- MacOS: This may be .dylib
 
