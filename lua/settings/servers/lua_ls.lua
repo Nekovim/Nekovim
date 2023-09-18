026b05c659dd867ac150eea0cd85e33c24d1c068
Niamh <Niamh.Ferns@proton.me>
2023-09-19 08:59:32 +1200

    EslintFixAll currently not working. Not sure why.

--- lua/settings/servers/lua_ls.lua
+++ lua/settings/servers/lua_ls.lua
@@ -1,4 +1,8 @@
 return {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    require("settings.servers").default_on_attach(client, bufnr)
+  end,
   settings = {
     Lua = {
       diagnostics = {
