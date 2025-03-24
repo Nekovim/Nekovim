fe7c63d18301445e5b7172da8f3d32e8fd3051af
Niamh <Niamh.K.Ferns@gmail.com>
2025-03-24 20:58:32 +1300

    Added ufo

---
+++ lua/modules/core/flow/ufo.lua
@@ -0,0 +1,25 @@
+local M = {
+  "kevinhwang91/nvim-ufo",
+  event = "VimEnter",
+  dependencies = {
+    {
+      "kevinhwang91/promise-async",
+    },
+  },
+}
+
+function M.config(_, opts)
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.opt.foldtext = ""
+  vim.opt.fillchars:append "fold: "
+  vim.opt.foldlevelstart = 99
+
+  require("ufo").setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+  })
+end
+
+return M
